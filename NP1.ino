96c8fecaccd80a857197d76d17f0333ad9643f16
Kogitare <martinferreiraalejandro@gmail.com>
2026-09-06 16:39:53 -0300

    Update NP1.ino

--- NP1.ino
+++ NP1.ino
@@ -3,20 +3,25 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-// 1. Configuracion de Hardware
 const uint8_t PIN_TRIG    = 5;
 const uint8_t PIN_ECHO    = 18;
 const uint8_t PIN_SERVO   = 19;
 const uint8_t PIN_BOTON   = 25; 
 const uint8_t PIN_LED_AL  = 33; 
 const uint8_t PIN_BUZZER  = 27; 
+//Aplicar correcion a M y B 07-09
+const float M_CAL = 1.0; 
+const float B_CAL = 0.0; 
+
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 const uint8_t DIR_OLED = 0x3C;
 bool bus_ok = false;
+
 const bool     BUZZER_PASIVO  = true; 
 const uint32_t FREC_BUZZER_HZ = 2000; 
+
 const uint8_t ANGULO_MIN  = 0;
 const uint8_t ANGULO_MAX  = 180;
 const uint8_t PASO_ANGULO = 5;
@@ -46,7 +51,7 @@ const char* nombreEstado(Estado e) {
   return "?";
 }
 
-void cambiar(Estado e, const char* motivo) {
+void cambiar(Estado e) {
   estado    = e;
   t_entrada = millis();
 }
@@ -88,53 +93,65 @@ float medirDistancia() {
 void mostrarOLED(int ang, float dist, Estado est) {
   if (!bus_ok) return;
   display.clearDisplay();
+  
   display.setTextSize(1);
   display.setCursor(0, 0);
   display.printf("ESTADO: %s\n", nombreEstado(est));
+  
   display.setTextSize(2);
   display.setCursor(0, 16);
   display.printf("Ang: %d\n", ang);
+  
   display.setCursor(0, 36);
   if (dist < 0) {
     display.println("D: ERROR");
   } else {
     display.printf("D: %.1f cm\n", dist);
   }
+  
   display.setTextSize(1);
   display.setCursor(0, 56);
   display.printf("BUS: I2C %s", bus_ok ? "OK" : "ERROR");
+  
   display.display();
 }
 
 void setup() {
   Serial.begin(115200);
+  
   Wire.begin(21, 22);
   Wire.setClock(400000); 
+  
   if(!display.begin(SSD1306_SWITCHCAPVCC, DIR_OLED)) {
     bus_ok = false;
   } else {
     bus_ok = true;
     display.setTextColor(WHITE);
   }
+
   pinMode(PIN_TRIG, OUTPUT);
   pinMode(PIN_ECHO, INPUT);
   pinMode(PIN_BOTON, INPUT_PULLDOWN); 
   pinMode(PIN_LED_AL, OUTPUT);
   pinMode(PIN_BUZZER, OUTPUT);
+
   miServo.setPeriodHertz(50);
   miServo.attach(PIN_SERVO, 500, 2400);
   miServo.write(angulo);
+
   Serial.println("t_ms,angulo,distancia_cm,estado,bus_ok"); 
-  cambiar(ASENTAMIENTO, "arranque");
+
+  cambiar(ASENTAMIENTO);
 }
 
 void loop() {
   switch (estado) {
+
     case BARRIDO:
       buzzer(false);
       digitalWrite(PIN_LED_AL, LOW);
       angulo += (PASO_ANGULO * direccion);
-
+      
       if (angulo >= ANGULO_MAX) {
         angulo = ANGULO_MAX;
         direccion = -1;
@@ -144,45 +161,49 @@ void loop() {
       }
       
       miServo.write(angulo);
-      cambiar(ASENTAMIENTO, "movimiento iniciado");
+      cambiar(ASENTAMIENTO);
       break;
 
     case ASENTAMIENTO:
       buzzer(false);
       if (millis() - t_entrada >= T_ASENTAMIENTO_MS) {
-        cambiar(MEDICION, "asentamiento mecanico completo");
+        cambiar(MEDICION);
       }
       break;
 
     case MEDICION:
       {
         buzzer(false);
-        ultima_dist = medirDistancia();
-
-        if (ultima_dist < 0) {
+        float distancia_cruda = medirDistancia();
+        
+        if (distancia_cruda < 0) {
           invalidas++;
           if (invalidas >= MAX_INVALIDAS) {
-            cambiar(ERROR_SEGURO, "sensor: 3 lecturas HC-SR04 invalidas");
+            cambiar(ERROR_SEGURO);
           } else {
-            cambiar(BARRIDO, "lectura invalida aislada");
+            cambiar(BARRIDO);
           }
         } else {
           invalidas = 0;
+          
+          ultima_dist = (distancia_cruda * M_CAL) + B_CAL; 
+          
           Serial.printf("%lu,%d,%.1f,%s,%u\n", millis(), angulo, ultima_dist, nombreEstado(estado), bus_ok ? 1 : 0);
           
-          cambiar(ACTUALIZAR_PANTALLA, "medicion exitosa");
+          cambiar(ACTUALIZAR_PANTALLA);
         }
       }
       break;
 
     case ACTUALIZAR_PANTALLA:
       mostrarOLED(angulo, ultima_dist, estado);
-      cambiar(BARRIDO, "pantalla actualizada");
+      cambiar(BARRIDO);
       break;
 
     case ERROR_SEGURO:
       digitalWrite(PIN_LED_AL, (millis() / 300) % 2);
       buzzer((millis() / 300) % 2); 
+      
       static uint32_t t_oled_error = 0;
       if (millis() - t_oled_error > 500) {
         mostrarOLED(angulo, -1.0, estado);
@@ -193,13 +214,14 @@ void loop() {
       if (boton()) {
         invalidas = 0;
         buzzer(false); 
-        cambiar(BARRIDO, "boton: REARME");
+        cambiar(BARRIDO);
       }
       break;
   }
+
   if (estado != ERROR_SEGURO) {
     if (boton()) {
-      cambiar(ERROR_SEGURO, "boton: PARO");
+      cambiar(ERROR_SEGURO);
     }
   }
 }
