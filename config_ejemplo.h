457c3df0b47e21bb2355715a028b80fc02a22dca
Kogitare <martinferreiraalejandro@gmail.com>
2026-09-26 00:55:13 -0300

    Create config_ejemplo.h

---
+++ config_ejemplo.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define WIFI_SSID  "RED_WIFI"
+#define WIFI_PASS  "CONTRASENA"
+
+#define MQTT_HOST  "192.168.x.x"
+#define MQTT_PORT  1883
+
+#define MQTT_USER  "usuario_mqtt"
+#define MQTT_PASS  "contrasena_mqtt"
+
+#define PROYECTO   "P14"
+#define NODO       "nodo1"
